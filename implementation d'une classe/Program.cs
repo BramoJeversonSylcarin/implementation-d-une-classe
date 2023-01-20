e093188164ec148373da0e0819e5d7c2cabdcc2e
ASUS <ASUS@DESKTOP-C1ULOQT>
2023-01-20 23:07:52 +0100

    Ajoutez des fichiers projet.

---
+++ implementation d'une classe/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implementation_d_une_classe
+{
+    using System;
+
+    public class Bibliotheque
+    {
+        public char commanderLivre()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        public char enregistrerLivre()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        private char nom;
+        private int adresse;
+        private char employe;
+        private char livre;
+
+    }
+
+    public class Livre
+    {
+        public char emprunter()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        public int prolonger()
+        {
+            // TODO: implement
+            return 0;
+        }
+
+        public char restaurer()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        private char titre;
+        private char auteur;
+        private int numero;
+
+    }
+    public class Employe
+    {
+        public char enregistrementLivre()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        public char indexerDesLivre()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        private char nom;
+        private char poste;
+
+    }
+    public class Emprunteur
+    {
+        public char retar()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        public char suspension()
+        {
+            // TODO: implement
+            return '0';
+        }
+
+        private char nom;
+        private int numero;
+        private int duree;
+
+    }
+
+    public class Archive
+    {
+        private DateTime dateArchive;
+        private char nom;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
